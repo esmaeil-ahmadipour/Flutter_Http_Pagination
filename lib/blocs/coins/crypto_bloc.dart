ad229790acb9d8ab76eb6453ba79ced0eaf2b15b
Esmaeil Ahmadipour <software8686@gmail.com>
2020-05-09 00:14:58 +0430

    commit> Add Bloc

--- lib/blocs/coins/crypto_bloc.dart
+++ lib/blocs/coins/crypto_bloc.dart
@@ -1,8 +1,20 @@
 import 'dart:async';
+
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterapipagination/models/coin_model.dart';
+import 'package:flutterapipagination/repositories/repository.dart';
+import 'package:meta/meta.dart';
+
 import './bloc.dart';
 
 class CryptoBloc extends Bloc<CryptoEvent, CryptoState> {
+  final CryptoRepository _cryptoRepository;
+
+  CryptoBloc({@required CryptoRepository cryptoRepository})
+      : assert(cryptoRepository != null),
+        _cryptoRepository = cryptoRepository;
+
   @override
   CryptoState get initialState => InitialCryptoState();
 
@@ -10,6 +22,34 @@ class CryptoBloc extends Bloc<CryptoEvent, CryptoState> {
   Stream<CryptoState> mapEventToState(
     CryptoEvent event,
   ) async* {
-    // TODO: Add Logic
+    if (event is AppStarted) {
+      yield* _mapAppStartedToState();
+    } else if (event is RefreshCoins) {
+      yield* _getCoins(coins: []);
+    } else if (event is LoadMoreCoins) {
+      yield* _mapLoadMoreCoinsState(event);
+    }
+  }
+
+  Stream<CryptoState> _getCoins({List<Coin> coins, int page = 0}) async* {
+    //Request Coins
+
+    try {
+      List<Coin> newCoinList=coins+ await _cryptoRepository.getTopCoins(page: page);
+      yield CryptoLoaded(coins: newCoinList);
+
+    } catch (error) {
+      yield CryptoError();
+    }
+  }
+
+  Stream<CryptoState> _mapAppStartedToState() async*{
+    yield CryptoLoading();
+    yield* _getCoins(coins: []);
+  }
+
+  Stream<CryptoState>  _mapLoadMoreCoinsState(LoadMoreCoins event)async* {
+    final int nextPage= event.coins.length ~/ CryptoRepository.perPage;
+    yield* _getCoins(coins: event.coins,page: nextPage);
   }
 }
