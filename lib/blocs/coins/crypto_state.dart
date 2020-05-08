f0a29feefce0a37ab75679cff945fad6b51db385
Esmaeil Ahmadipour <software8686@gmail.com>
2020-05-08 22:37:18 +0430

    commit> Add State's

--- lib/blocs/coins/crypto_state.dart
+++ lib/blocs/coins/crypto_state.dart
@@ -1,4 +1,5 @@
 import 'package:equatable/equatable.dart';
+import 'package:flutterapipagination/models/coin_model.dart';
 
 abstract class CryptoState extends Equatable {
   const CryptoState();
@@ -9,3 +10,24 @@ class InitialCryptoState extends CryptoState {
   List<Object> get props => [];
 }
 
+class CryptoLoading extends CryptoState {
+  @override
+  List<Object> get props => [];
+}
+
+class CryptoLoaded extends CryptoState {
+  final List<Coin> coins;
+
+  const CryptoLoaded({this.coins});
+
+  @override
+  List<Object> get props => [coins];
+
+  @override
+  String toString() => 'CryptoLoaded {coins: $coins}';
+}
+
+class CryptoError extends CryptoState {
+  @override
+  List<Object> get props => [];
+}
